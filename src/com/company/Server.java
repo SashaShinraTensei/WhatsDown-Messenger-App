436af8697aab4122c5ef907b88576be33b3de8aa
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2022-02-05 17:39:07 +0100

    upload working one way communication to branch

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -1,39 +1,66 @@
 package com.company;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-
     private ServerSocket serverSocket;
+    private Socket socket;
+    private BufferedReader sentData;
+    private PrintWriter writeToClient;
+    private int port;
 
-    public Server(ServerSocket serverSocket){
+    public Server(ServerSocket serverSocket) throws IOException {
+        System.out.println("Waiting for connection!");
         this.serverSocket = serverSocket;
     }
 
-    public void startServer() {
-        try {
-            while (!serverSocket.isClosed()){
-                Socket socket = serverSocket.accept();
-                System.out.println("A new Client has been connected");
-                ClientHandler clientHandler = new ClientHandler(socket);
-
-                Thread thread = new Thread(clientHandler);
-                thread.start();
-            }
-        }catch (IOException e){
-
+    public void startServer() throws IOException {
+        if (!this.serverSocket.isClosed()) {
+            this.socket = this.serverSocket.accept();
+            System.out.println("Client connected");
+            this.startStreams();
         }
+
     }
 
-    public void closeServer() {
-        try {
-            if (serverSocket!= null){
-                serverSocket.close();
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
+    public void startStreams() throws IOException {
+        this.sentData = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+        this.writeToClient = new PrintWriter(this.socket.getOutputStream(), true);
+    }
+
+    public void sentToClient(String message) throws IOException {
+        this.writeToClient.println(message);
+    }
+
+    public ServerSocket getServerSocket() {
+        return this.serverSocket;
+    }
+
+    public Socket getSocket() {
+        return this.socket;
+    }
+
+    public BufferedReader getSentData() {
+        return this.sentData;
+    }
+
+    public PrintWriter getWriteToClient() {
+        return this.writeToClient;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server(new ServerSocket(6969));
+        server.startServer();
+        String in = server.getSentData().readLine();
+        server.sentToClient(in);
     }
 }
